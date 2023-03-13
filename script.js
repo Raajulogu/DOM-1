079158441eff0026bfe93b61e717cc9e1472237f
Raajulogu <111479920+Raajulogu@users.noreply.github.com>
2023-03-13 12:27:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var firstname=document.getElementById("fname").value;
+    console.log(firstname);
+    var middlename=document.getElementById("mname").value;
+    console.log(middlename);
+    var lastname=document.getElementById("lname").value;
+    console.log(lastname);
+    var Email=document.getElementById("email").value;
+    console.log(Email);
+    var Pass=document.getElementById("password").value;
+    console.log(Pass);
+}
